d3951355e2477136c6a22c18932a7fe0807cee3f
Chris Loveless <chriskloveless@gmail.com>
2022-10-21 10:59:54 -0700

    add event listeners for UI logic and branches for business logic

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+//Event listener for loading page and running UI logic
+addEventListener("load", function() {
+  const form = document.querySelector("form")
+
+//add Event Listener to hold form values and convert to numbers
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const input1 = parseInt(document.getElementById("input1").value);
+    const input2 = parseInt(document.getElementById("input2").value);
+    const input3 = parseInt(document.getElementById("input3").value);
+    const input4 = parseInt(document.getElementById("input4").value);
+    const input5 = parseInt(document.getElementById("input5").value);
+    const total = input1 + input2 + input3 + input4 + input5;
+
+    let result = document.querySelector("span#result");
+
+
+
+
+//add event listener for reset button to reload page on click
+  const reset = document.getElementById("reset-btn");
+  reset.addEventListener("click", function() {
+    document.location.reload();
+  })
+
+
+//Business Logic
+if (total <= 4) {
+  document.querySelector("div#error").removeAttribute("class");
+}
+else if (total >=5 && total <= 10) {
+  document.querySelector("div#result").removeAttribute("class");
+  document.querySelector("span#output").innerText = "JavaScript";
+}
+else if (total > 10 && total <= 20) {
+  document.querySelector("div#result").removeAttribute("class");
+  document.querySelector("span#output").innerText = "Python";
+}
+else if (total > 20 && total <= 25) {
+  document.querySelector("div#result").removeAttribute("class");
+  document.querySelector("span#output").innerText = "Rust";
+}
+
+  })
+});
+
+
